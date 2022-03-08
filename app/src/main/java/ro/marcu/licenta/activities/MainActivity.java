f621f02f3e6d8f4699834a306bc3c66e51c19435
Axentii Marcel <marcel.axentii@mail.ru>
2022-03-08 13:34:26 +0200

    First commit

---
+++ app/src/main/java/ro/marcu/licenta/activities/MainActivity.java
@@ -0,0 +1,15 @@
+package ro.marcu.licenta.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ro.marcu.licenta.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
