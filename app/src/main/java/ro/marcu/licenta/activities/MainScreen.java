821251e0d8ce02fa0b5c29d945271f2fb915ebfe
Axentii Marcel <marcel.axentii@mail.ru>
2022-03-12 21:22:19 +0200

    Started making MainScreen and chart view

--- app/src/main/java/ro/marcu/licenta/activities/MainScreen.java
+++ app/src/main/java/ro/marcu/licenta/activities/MainScreen.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.github.mikephil.charting.charts.LineChart;
+
 import ro.marcu.licenta.R;
 
 public class MainScreen extends AppCompatActivity {
+
+    private LineChart mChart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
